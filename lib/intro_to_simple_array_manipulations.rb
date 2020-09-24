e995c0ae2f6f127f5715db1309ce324e23837165
akam alam <arundelfootball23@gmail.com>
2020-09-24 05:22:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,10 +21,11 @@ def using_flatten(array)
 s = [ 1, 2, 3 ]           
 t = [ 4, 5, 6 ] 
 a = [ s, t, 9, 10 ] 
-flat_array = array.flatten  
+flat_array = array.flatten.to_s  
 end 
 
 def using_delete(array, string)
+  
 
 end 
 
