5423ab58c2a4e48503b3207ae0e1c95d800e51f9
akam alam <arundelfootball23@gmail.com>
2020-09-24 05:30:45 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,37 @@
+def  using_concat(array, array1)
+  array.concat(array1) 
+  
+end 
+
+
+def using_insert(array,element)
+  
+  array.insert(-4, element)
+  
+end 
+
+
+def using_uniq(array)
+  array.uniq
+  
+end 
+
+
+def using_flatten(array)
+s = [ 1, 2, 3 ]           
+t = [ 4, 5, 6 ] 
+a = [ s, t, 9, 10 ] 
+flat_array = array.flatten 
+end 
+
+def using_delete(array, string)
+
+  array.delete(string) 
+  
+
+end 
+
+def using_delete_at(array, integer)
+  array.delete_at(integer) 
+
+end 
