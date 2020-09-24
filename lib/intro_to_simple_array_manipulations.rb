813879624205ccae22377f3d4085a9f426864b87
akam alam <arundelfootball23@gmail.com>
2020-09-24 05:05:58 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -17,13 +17,12 @@ def using_uniq(array)
 end 
 
 
-def usning_flatten(array)
+def using_flatten(array)
   x=1 
   y=2 
   m=3 
   array = [x, y, m]
- 
-flat_array = array.flatten 
+ array.flatten 
   
 end 
 
